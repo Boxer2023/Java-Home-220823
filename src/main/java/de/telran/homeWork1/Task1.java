62a6d0e510ef22d91200c31b47d00600fc68f2e3
LENOVO <dmit.mor@gmail.com>
2023-09-07 23:01:01 +0200

    Init Commit

---
+++ src/main/java/de/telran/homeWork1/Task1.java
@@ -0,0 +1,20 @@
+package de.telran.homeWork1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        char z = 'G';
+        System.out.print("char: " + z);
+        int x = 89;
+        System.out.print("int: " + x);
+        byte y = 4;
+        System.out.print("byte: " + y);
+        short i = 56;
+        System.out.print("short: " + i);
+        float a = 4.7333436f;
+        System.out.print("float: " + a);
+        double b = 4.355453532;
+        System.out.print("double: " + b);
+        long c = 12122;
+        System.out.print("long: " + c);
+    }
+}
