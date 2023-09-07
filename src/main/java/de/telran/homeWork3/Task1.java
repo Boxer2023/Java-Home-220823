0b9830f9e68df37a9c705d7e53383afc23567957
LENOVO <dmit.mor@gmail.com>
2023-09-07 23:02:30 +0200

    Init Commit

---
+++ src/main/java/de/telran/homeWork3/Task1.java
@@ -0,0 +1,18 @@
+package de.telran.homeWork3;
+
+public class Task1 {
+    public static void main(String[]  args) {
+        String text1 = new String("I Study Basic Java!");
+        forString(text1);
+        System.out.println(text1.charAt(17));
+        System.out.println(text1.contains("Java"));
+        System.out.println(text1.substring(0,13)+text1.substring(18));
+        System.out.println(text1.replace("a","o"));
+        System.out.println(text1.toUpperCase());
+        System.out.println(text1.toLowerCase());
+
+    }
+    public static void forString(String best){
+        System.out.println(best);
+    }
+}
